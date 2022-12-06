d77fd667546d164a0921fa52a8b39a24686ff75b
CBEngineer <CBEalexanderkevin@gmail.com>
2022-12-06 19:26:33 +0700

    add required on register users role

--- app/Http/Controllers/LoginRegis/RegisterController.php
+++ app/Http/Controllers/LoginRegis/RegisterController.php
@@ -19,7 +19,7 @@ class RegisterController extends Controller
             "users_password" => "required | confirmed",
             "users_alamat" => "required",
             "users_telepon" => "required | numeric | digits_between:8,12 | unique:users,users_telepon",
-            "users_role" => [Rule::in(["customer","provider"])],
+            "users_role" => ["required", Rule::in(["customer","provider"])],
             "tnc" => "accepted",
         ]);
 
